973fee72a6b84c47ac9a8ceefaf934cdc68dd3c2
alias <nitishkumarchauhan1996@gmail.com>
2021-08-24 22:25:19 +0530

    settings changes

--- studentInfo/studentInfo/urls.py
+++ studentInfo/studentInfo/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('linksgrab.urls', namespace="linksgrab")),
+    path('', include('accounts.urls', namespace="accounts")),
     path('', include('Students.urls', namespace="Students"))
 ]
